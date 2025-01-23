5e425aa10c9acf540904a5684d98af469edbdb85
scrin012 <scrin012@gmail.com>
2025-01-23 18:11:31 +0900

    250123

--- css_animation/example/2-3-star/script/index.js
+++ css_animation/example/2-3-star/script/index.js
@@ -1,14 +1,28 @@
-const menu1 = document.querySelector('.menu1');
-const menu2 = document.querySelector('.menu2');
-const menu3 = document.querySelector('.menu3');
+const category = document.querySelectorAll('.category_menu a')
+const menu = document.querySelectorAll('.menu_contents .menu');
 const menuTitle = document.querySelector('#menu_title');
 
-// =========================================== 초기세팅
-menu2.style.display = 'none';
-menu3.style.display = 'none';
+// =========================================== 초기세팅 / 메뉴 숨기기
+for(let i of menu){
+  hideMenu();
+  menu[0].style.display = 'flex';
+}
 
-function hideMenu(){
-  menu1.classList.remove('active');
-  menu2.classList.remove('active');
-  menu3.classList.remove('active');
+// =========================================== 함수
+function activeRemove(){
+  for(let i of category){
+    i.classList.remove('active');
+  }
 };
+function hideMenu(){
+  for(let i of menu){
+    i.style.display = 'none';
+  }
+}
+
+for(let i of category){
+  i.addEventListener('click',()=>{
+    activeRemove();
+    i.classList.add('active');
+  })
+}
