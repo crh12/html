736dc9e86056d11e70ae105e5ad48b48f1ffc46d
scrin012 <scrin012@gmail.com>
2025-02-10 13:26:37 +0900

    250210 오전

--- portfolio/giordano/README.md
+++ portfolio/giordano/README.md
@@ -3,16 +3,21 @@
 ### 배너 대제목 44
 font-size: 2.75rem; font-weight: 500; letter-spacing: -0.02em;
 ### 메인 제목 35
+font-size: 2.19rem; font-weight: 700; letter-spacing: -0.02em;
 ### 배너 중제목 32
 ### 상세 제목 28 강조
 ### 상세 제목 28
 ### 배너 내용 20
 font-size: 1.25rem; letter-spacing: -0.02em;
 ### 메인 중제목 17
+font-size: 1.06rem; letter-spacing: -0.02em;
 ### 헤더 17
+font-size: 1.06rem; font-weight: 500; letter-spacing: -0.02em;
 ### 헤더 16
 ### 메인 내용 16 강조
+font-weight: 500; letter-spacing: -0.02em;
 ### 메인 내용 16
+letter-spacing: -0.02em;
 ### 상세 내용 15
 ### 상세 내용 14 강조
 ### 상세 내용 14
--- portfolio/giordano/scripts/index.js
+++ portfolio/giordano/scripts/index.js
@@ -1,8 +1,47 @@
+const mainSlide = document.querySelectorAll('.main_swiper .swiper-slide');
+const mainSlideTotal = document.querySelector('.slide_total');
+const mainSlideCurrent = document.querySelector('.slide_current');
+
+mainSlideTotal.textContent = mainSlide.length;
+
 const mainSwiper = new Swiper('.main_swiper',{
   // autoplay:{delay:2500,},
   loop:true,
-  // navigation:{
-  //   nextEl:'.progress_btn .next',
-  //   prevEl:'.progress_btn .prev',
-  // }
+  navigation:{
+    nextEl:'.progress_btn .next',
+    prevEl:'.progress_btn .prev',
+  },
+  on:{
+    slideChange:function(){
+      mainSlideCurrent.textContent = this.realIndex+1;
+    }
+  },
+});
+
+const bestSwiper = new Swiper('.best_swiper1',{
+  loop:true,
+  navigation:{
+    nextEl:'.best_slide_btn .next',
+    prevEl:'.best_slide_btn .prev',
+  },
+  slidesPerView:5,
+  spaceBetween:25,
+});
+
+const newSwiper = new Swiper('.new_swiper1',{
+  loop:true,
+  navigation:{
+    nextEl:'.new_slide_btn .next',
+    prevEl:'.new_slide_btn .prev',
+  },
+  slidesPerView:5,
+  spaceBetween:25,
+});
+
+const mdSwiper = new Swiper('.md_swiper',{
+  loop:true,
+  navigation:{
+    nextEl:'.md_slide_btn .next',
+    prevEl:'.md_slide_btn .prev',
+  }
 });
