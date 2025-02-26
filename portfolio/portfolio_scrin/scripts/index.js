eadd7a78010d34420308a3c9d49c7c1fcc41ad08
scrin012 <scrin012@gmail.com>
2025-02-26 18:53:08 +0900

    250226

--- portfolio/portfolio_scrin/scripts/index.js
+++ portfolio/portfolio_scrin/scripts/index.js
@@ -1,6 +1,32 @@
 const mSwiper = new Swiper ('.m_swiper', {
   direction: 'vertical',
   mousewheel: true,
-  // autoplay:{delay:0,},
-  // speed:4000,
 });
+
+const snsSwiper = new Swiper ('.sns_swiper', {
+  autoplay:{delay:0,},
+  loop:true,
+  speed:6000,
+  slidesPerView:3,
+});
+
+const bnrSwiper = new Swiper ('.bnr_swiper', {
+  autoplay:{delay:0,},
+  loop:true,
+  speed:6000,
+  slidesPerView:3,
+});
+
+const roundTxt = document.querySelector('.round_txt_wrap');
+
+console.log(mSwiper.activeIndex)
+
+window.addEventListener('scroll',()=>{
+  console.log('스크롤')
+})
+// mSwiper.activeIndex
+// mSwiper.addEventListener('activeIndexChange',()=>{
+//   console.log('스크롤')
+// })
+// if (mSwiper.activeIndex = '1') console.log(mSwiper.activeIndex)
+// if (mSwiper.activeIndex == '1') roundTxt.style.right == '-117px'
