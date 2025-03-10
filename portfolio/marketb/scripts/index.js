6b9078b1a51ecec793cca21a50445224dea3c788
scrin012 <scrin012@gmail.com>
2025-03-10 18:11:13 +0900

    250310

--- portfolio/marketb/scripts/index.js
+++ portfolio/marketb/scripts/index.js
@@ -1,11 +1,13 @@
 const mainSlideCurrent = document.querySelector('.main_bnr .swiper_current');
 const mainSlideTotal = document.querySelector('.main_bnr .swiper_total');
 
+
+mainSlideTotal.textContent = `0${document.querySelectorAll('.main_bnr .swiper-slide').length}`;
+
 const mainSwiper = new Swiper('.main_swiper',{
-    autoplay:{delay:2500,},
+    // autoplay:{delay:2500,},
     loop:true,
     spaceBetween:50,
-    slidesPerView:1.1,
     centeredSlides:true,
     navigation:{
         nextEl:'.main_bnr .next',
@@ -13,11 +15,19 @@ const mainSwiper = new Swiper('.main_swiper',{
     },
     on:{
         slideChange:function(){
-        mainSlideCurrent.textContent = this.realIndex+1;
+        mainSlideCurrent.textContent = `0${this.realIndex+1}`;
         }
     },
-    pagination: {
-        el: '.main_bnr .swiper-pagination',
-        type: 'progressbar',
+    scrollbar: {
+        el: '.main_bnr .swiper-scrollbar',
     },
+});
+
+const roomSwiper = new Swiper('.room_swiper',{
+    loop:true,
+    spaceBetween:30,
+    // navigation:{
+    //     nextEl:'.main_bnr .next',
+    //     prevEl:'.main_bnr .prev',
+    // },
 });
